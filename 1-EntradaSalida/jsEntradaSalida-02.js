0c7dabab82faa9c92811f6a2e74c051d84b1cf90
julio-urquiza+ <julio.cesar.urquiza4@gmail.com>
2020-07-13 22:41:16 -0300

    clase 1

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,13 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre; //declaro la variable nombre
+	/*nombre ="julio";
+	alert(nombre);
+	nombre= "urquiza";*/
+	nombre = prompt("ingrese su nombre");
+	alert(nombre);
+
+
 }
 
