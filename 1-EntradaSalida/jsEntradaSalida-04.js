03ec945fa60566ca55f3c3063b098e5b004d0074
julio-urquiza+ <julio.cesar.urquiza4@gmail.com>
2020-07-14 23:00:13 -0300

    cambios

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,15 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reservo espacio en memoria 
+	let nombre;
+	//guardo  en la variable nombre el texto que escribio en usuario dentro de la ventana prompt
+	nombre = prompt("ingrese su nombre");
+	//copio en nombre que tengo guardado en la variable nombre dentro de la caja de texto enmbebida en la pagina 
+	document.getElementById("txtIdNombre").value = nombre
+
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let nun1;
+	let nun2;
+	let resultado;
+	
+	nun1 =parseInt(document.getElementById("txtIdNumeroUno").value);
+	//tranforma de string a number  (tambien esta parsefloat)
+	nun2 =parseInt(document.getElementById("txtIdNumeroDos").value);
+	resultado = nun1 + nun2;
+	alert ("la suma es "+ resultado);
 }
 
