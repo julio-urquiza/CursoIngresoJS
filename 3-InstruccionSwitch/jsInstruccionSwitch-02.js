f03076ea29f821816fe3513f5bd39fb7d3a90bc7
julio-urquiza+ <julio.cesar.urquiza4@gmail.com>
2020-08-12 13:19:07 -0300

    terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,6 +1,36 @@
 function mostrar()
 {
-	
+    /*
+    al seleccionar un mes informar.
+    si estamos en Invierno: "Abrigate que hace frio."
+    si aún no llego el Invierno: "Falta para el invierno."
+    si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+    ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno. 
+    */
+	let mes;
+	mes = document.getElementById("txtIdMes").value;
+
+	switch(mes)
+	{
+        case "Enero": 
+		case "Enero": 
+		case "Marzo":
+		case "Enero": 
+        case "Enero": 
+		case "Enero": 
+			alert("Falta para el invierno");
+			break;
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace frio");
+			break;
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+	}
 
 
 
